755dce4999be020b2a181764c63ea99e9b910a37
ooiws <ooiwensong@gmail.com>
2024-02-26 16:52:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,20 @@ let bag;
 const tileColours = ["blue", "yellow", "red", "black", "white"];
 const forIteration = ["one", "two", "three", "four", "five"];
 const floorScoreLookup = {
-  '0': 0,
-  '1': 1,
-  '2': 2,
-  '3': 4,
-  '4': 6,
-  '5': 8,
-  '6': 11,
-  '7': 14,
-}
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 4,
+  4: 6,
+  5: 8,
+  6: 11,
+  7: 14,
+};
 
 /*----- element references -----*/
 const factories = document.querySelector("#factories");
 const factoriesArray = document.querySelectorAll(".factory");
-const hand = document.querySelector("#hand")
+const hand = document.querySelector("#hand");
 const table = document.querySelector("#table");
 const lineContainer = document.querySelector("#line-container");
 const lines = document.querySelectorAll(".line");
@@ -40,7 +40,7 @@ function initialise() {
     { red: 20 },
     { black: 20 },
     { white: 20 },
-  ]
+  ];
   setupFactories();
   renderInfo();
 }
@@ -129,10 +129,14 @@ function wallTiling() {
     }
     // if line is complete, append the first position piece to corresponding wall tile
     const currentLineRow = line.classList[1];
-    const currentWallRow = document.querySelectorAll(`.wall-tile.${currentLineRow}`);
+    const currentWallRow = document.querySelectorAll(
+      `.wall-tile.${currentLineRow}`
+    );
     const firstTile = line.firstElementChild.firstElementChild;
     const firstTileColour = firstTile.classList[1];
-    const currentWallTile = [...currentWallRow].find((square) => square.classList.contains(firstTileColour));
+    const currentWallTile = [...currentWallRow].find((square) =>
+      square.classList.contains(firstTileColour)
+    );
     firstTile.setAttribute("draggable", false);
     currentWallTile.append(firstTile);
     wallTileScoring(currentWallTile);
@@ -230,14 +234,20 @@ function finalScoring() {
   // Checks for 5 of a kind in the wall
   for (const colour of tileColours) {
     const currentSquares = document.querySelectorAll(`.wall-tile.${colour}`);
-    [...currentSquares].every((square) => square.firstElementChild) ? score += 10 : score += 0;
+    [...currentSquares].every((square) => square.firstElementChild)
+      ? (score += 10)
+      : (score += 0);
   }
   // Checks for complete columns and rows
   for (const num of forIteration) {
     const allColumns = document.querySelectorAll(`.wall-tile.column-${num}`);
     const allRows = document.querySelectorAll(`.wall-tile.row-${num}`);
-    [...allColumns].every((square) => square.firstElementChild) ? score += 7 : score += 0;
-    [...allRows].every((square) => square.firstElementChild) ? score += 2 : score += 0;
+    [...allColumns].every((square) => square.firstElementChild)
+      ? (score += 7)
+      : (score += 0);
+    [...allRows].every((square) => square.firstElementChild)
+      ? (score += 2)
+      : (score += 0);
   }
 }
 
@@ -247,19 +257,22 @@ function dummyTurn() {
   let largestSetIndex;
   let largestSetColour;
   // if factories and table are empty, do nothing
-  if (![...factoriesArray].find((factory) => factory.children.length > 0) && table.children.length === 0) {
+  if (
+    ![...factoriesArray].find((factory) => factory.children.length > 0) &&
+    table.children.length === 0
+  ) {
     return;
   }
   // if all factories are empty, take a random tile from table
   if (![...factoriesArray].find((factory) => factory.children.length > 0)) {
-    console.log("factories are empty!")
+    console.log("factories are empty!");
     const randomPieceIndex = Math.floor(Math.random() * table.children.length);
     const randomPieceColour = table.children[randomPieceIndex].classList[1];
     [...table.children].forEach((tile) => {
       if (tile.classList[1] === randomPieceColour) {
         tile.remove();
       }
-    })
+    });
     infoMsg = `Dummy removed a random set of ${randomPieceColour} pieces from the table`;
     renderInfo();
     return;
@@ -271,81 +284,104 @@ function dummyTurn() {
       continue;
     }
     const factoryState = [...factory.children].reduce((a, piece) => {
-      a[piece.classList[1]] ? a[piece.classList[1]] += 1 : a[piece.classList[1]] = 1;
+      a[piece.classList[1]]
+        ? (a[piece.classList[1]] += 1)
+        : (a[piece.classList[1]] = 1);
       return a;
-    }, {})
+    }, {});
     factoriesState.push(factoryState);
   }
   // Checks if any factory has a set of 4
   if (factoriesState.find((factory) => Object.values(factory).includes(4))) {
-    largestSetIndex = factoriesState.findIndex((factory) => Object.values(factory).includes(4));
-    largestSetColour = factoriesArray[largestSetIndex].firstElementChild.classList[1];
+    largestSetIndex = factoriesState.findIndex((factory) =>
+      Object.values(factory).includes(4)
+    );
+    largestSetColour =
+      factoriesArray[largestSetIndex].firstElementChild.classList[1];
     [...factoriesArray[largestSetIndex].children].forEach((tile) => {
       tile.remove();
-    })
+    });
     infoMsg = `Dummy removed a set of 4 ${largestSetColour} pieces from the factory`;
     renderInfo();
     return;
   }
-  // Checks if any factory has a set of 3 
+  // Checks if any factory has a set of 3
   if (factoriesState.find((factory) => Object.values(factory).includes(3))) {
-    largestSetIndex = factoriesState.findIndex((factory) => Object.values(factory).includes(3));
-    largestSetColour = Object.keys(factoriesState[largestSetIndex]).find((key) => factoriesState[largestSetIndex][key] === 3);
+    largestSetIndex = factoriesState.findIndex((factory) =>
+      Object.values(factory).includes(3)
+    );
+    largestSetColour = Object.keys(factoriesState[largestSetIndex]).find(
+      (key) => factoriesState[largestSetIndex][key] === 3
+    );
     [...factoriesArray[largestSetIndex].children].forEach((tile) => {
       if (tile.classList.contains(largestSetColour)) {
         tile.remove();
       } else {
         table.append(tile);
       }
-    })
+    });
     infoMsg = `Dummy removed a set of 3 ${largestSetColour} pieces from the factory`;
     renderInfo();
     return;
   }
-  // Checks if any factory has a set of 2 
+  // Checks if any factory has a set of 2
   if (factoriesState.find((factory) => Object.values(factory).includes(2))) {
-    largestSetIndex = factoriesState.findIndex((factory) => Object.values(factory).includes(2));
-    largestSetColour = Object.keys(factoriesState[largestSetIndex]).filter((key) => factoriesState[largestSetIndex][key] === 2);
-    const randomColour = largestSetColour[Math.floor(Math.random() * largestSetColour.length)];
+    largestSetIndex = factoriesState.findIndex((factory) =>
+      Object.values(factory).includes(2)
+    );
+    largestSetColour = Object.keys(factoriesState[largestSetIndex]).filter(
+      (key) => factoriesState[largestSetIndex][key] === 2
+    );
+    const randomColour =
+      largestSetColour[Math.floor(Math.random() * largestSetColour.length)];
     [...factoriesArray[largestSetIndex].children].forEach((tile) => {
       if (tile.classList.contains(randomColour)) {
         tile.remove();
       } else {
         table.append(tile);
       }
-    })
+    });
     infoMsg = `Dummy removed a set of 2 ${randomColour} pieces from the factory`;
     renderInfo();
     return;
   }
   // Create an object to document number of each coloured piece in the table
   const tableState = [...table.children].reduce((a, piece) => {
-    a[piece.classList[1]] ? a[piece.classList[1]] += 1 : a[piece.classList[1]] = 1;
+    a[piece.classList[1]]
+      ? (a[piece.classList[1]] += 1)
+      : (a[piece.classList[1]] = 1);
     return a;
-  }, {})
+  }, {});
   // Find the colour and quantity of the largest set in the table
   const largestSetQty = Math.max(...Object.values(tableState));
   // if there are only sets of ones in the table OR if there are no pieces in the table
   if (largestSetQty <= 1) {
-    const leftmostFactory = [...factoriesArray].find((factory) => factory.children.length > 0);
+    const leftmostFactory = [...factoriesArray].find(
+      (factory) => factory.children.length > 0
+    );
     const randomTile = leftmostFactory.children[Math.floor(Math.random() * 4)];
     randomTile.remove();
     [...leftmostFactory.children].forEach((tile) => {
       table.append(tile);
       infoMsg = `Dummy removed a ${randomTile.classList[1]} piece from the leftmost available factory`;
       renderInfo();
-    })
+    });
     // if there are sets of >1 in the table
   } else {
-    const largestTableSetsColours = Object.keys(tableState).filter((key) => tableState[key] === largestSetQty);
-    const randomColour = largestTableSetsColours[Math.floor(Math.random() * largestTableSetsColours.length)];
+    const largestTableSetsColours = Object.keys(tableState).filter(
+      (key) => tableState[key] === largestSetQty
+    );
+    const randomColour =
+      largestTableSetsColours[
+        Math.floor(Math.random() * largestTableSetsColours.length)
+      ];
     [...table.children].forEach((tile) => {
       if (tile.classList.contains(randomColour)) {
         tile.remove();
         infoMsg = `Dummy removed ${randomColour} pieces from the table`;
         renderInfo();
       }
-    })
+    });
   }
 }
 
@@ -383,9 +419,9 @@ factories.addEventListener("click", (e) => {
     } else {
       table.append(tile);
     }
-  })
+  });
   hasChosenFactory = true;
-})
+});
 
 table.addEventListener("click", (e) => {
   if (hasChosenFactory === true) {
@@ -403,39 +439,49 @@ table.addEventListener("click", (e) => {
       hand.append(tile);
       tile.setAttribute("draggable", true);
     }
-  })
+  });
   hasChosenFactory = true;
-})
+});
 
 let draggedPiece;
 hand.addEventListener("dragstart", (e) => {
   draggedPiece = e.target;
-})
+});
 lineContainer.addEventListener("dragover", (e) => {
   e.preventDefault();
-})
+});
 lineContainer.addEventListener("drop", (e) => {
   // e.stopPropagation();
   const targetRow = e.target.parentNode;
   const targetRowName = targetRow.classList[1];
-  const targetWallRow = document.querySelectorAll(`.wall-tile.${targetRowName}`);
+  const targetWallRow = document.querySelectorAll(
+    `.wall-tile.${targetRowName}`
+  );
   const firstPosition = e.target.parentNode.children[0];
   const draggedPieceColour = draggedPiece.classList[1];
 
   // If space is occupied,  piece cannot be dropped; piece cannot be dropped outside of grids
-  if (e.target.classList.contains("piece") || e.target.classList.contains("line")) {
+  if (
+    e.target.classList.contains("piece") ||
+    e.target.classList.contains("line")
+  ) {
     infoMsg = "Not a valid space";
     renderInfo();
     return;
   }
   // Cannot drop piece in a row with existing pieces of different colours
-  if (firstPosition.firstElementChild && firstPosition.firstElementChild.classList[1] !== draggedPieceColour) {
+  if (
+    firstPosition.firstElementChild &&
+    firstPosition.firstElementChild.classList[1] !== draggedPieceColour
+  ) {
     infoMsg = "Cannot drop into line containing pieces of a different colour";
     renderInfo();
     return;
   }
   // Cannot drop piece if wall contains the same colour
-  const targetWallTile = [...targetWallRow].find((square) => square.classList.contains(draggedPieceColour));
+  const targetWallTile = [...targetWallRow].find((square) =>
+    square.classList.contains(draggedPieceColour)
+  );
   if (targetWallTile.firstElementChild) {
     infoMsg = "Wall already contains tile of the same colour.";
     renderInfo();
@@ -448,7 +494,10 @@ lineContainer.addEventListener("drop", (e) => {
       break;
     }
     // if square contains a piece, move on to the next empty square
-    if (square.firstElementChild && square.firstElementChild.classList.contains("piece")) {
+    if (
+      square.firstElementChild &&
+      square.firstElementChild.classList.contains("piece")
+    ) {
       continue;
     } else {
       hand.firstElementChild.setAttribute("draggable", false);
@@ -486,7 +535,11 @@ document.querySelector(".pass").addEventListener("click", () => {
 
 document.querySelector(".next").addEventListener("click", () => {
   // Disallow users to advance if there are pieces still in factories/ hand/ table
-  if (hand.children.length > 0 || table.children.length > 0 || [...factoriesArray].find((factory) => factory.children.length > 0)) {
+  if (
+    hand.children.length > 0 ||
+    table.children.length > 0 ||
+    [...factoriesArray].find((factory) => factory.children.length > 0)
+  ) {
     return;
   }
   if (round === 5) {
@@ -501,7 +554,7 @@ document.querySelector(".next").addEventListener("click", () => {
   removePieces();
   setupFactories();
   round += 1;
-  console.log(bag)
+  console.log(bag);
   renderInfo();
 });
 
@@ -509,7 +562,7 @@ document.querySelector(".play-again").addEventListener("click", () => {
   for (const line of lines) {
     [...line.children].forEach((square) => {
       if (square.firstElementChild) {
-        square.firstElementChild.remove()
+        square.firstElementChild.remove();
       }
     });
   }
@@ -519,6 +572,10 @@ document.querySelector(".play-again").addEventListener("click", () => {
       square.firstElementChild.remove();
     }
   }
+  for (const square of floor) {
+    if (square.firstElementChild) {
+      square.firstElementChild.remove();
+    }
+  }
   initialise();
-})
-
+});
